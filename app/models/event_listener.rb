cd17585b1109b198b1aa5fd0b9831035dcd92781
Kevin McHugh <kev@kevinmchugh.me>
2014-06-13 10:53:08 -0500

    make the game end appropriately

--- app/models/event_listener.rb
+++ app/models/event_listener.rb
@@ -12,7 +12,28 @@ class EventListener
   end
 
   def notify(event)
-    @logger.info(event)
+    @logger.info(event.class)
+    if event.player_killed?
+      if event.killed.sheriff? || sheriff_win?
+        GameOverEvent.new(self, event)
+      end
+    end
     subscribers.each {|sub| sub.notify(event)}
   end
+
+
+  def sheriff_win?
+    living_players = game.players.reject(&:dead?)
+    roles = living_players.map(&:role)
+    !(roles.include?('outlaw') || roles.include?('renegade'))
+  end
+end
+
+class GameOverEvent < Event
+  attr_reader :player_killed_event
+  def initialize(event_listener, player_killed_event)
+    @player_killed_event = player_killed_event
+    super(event_listener)
+  end
+  def game_over?; true; end
 end
