81a2b01a9955565cc2bc77c806020c065cbebd85
Kevin McHugh <kev@kevinmchugh.me>
2014-06-13 10:54:52 -0500

    call methods that exist

--- app/models/cards.rb
+++ app/models/cards.rb
@@ -2,19 +2,19 @@
 class SpringfieldCard < Card
   def no_range?;true;end
   def play(player, target_player, target_card=nil)
-    target_player.target_of(self, player)
+    target_player.target_of_bang(self, player)
   end
 end
 # Dodge City
 class PunchCard < Card
   def play(player, target_player, target_card=nil)
-    target_player.target_of(self, player)
+    target_player.target_of_bang(self, player)
   end
 end
 class BangCard < Card
   def gun_range?;true;end
   def play(player, target_player, target_card=nil)
-    target_player.target_of(self, player)
+    target_player.target_of_bang(self, player)
   end
 end
 class BeerCard < Card
@@ -46,7 +46,7 @@ class GatlingCard < Card
     target_player = player.left
 
     while target_player != player
-      target_player.target_of(BangCard.new(self.suit, self.number), player)
+      target_player.target_of_bang(BangCard.new(self.suit, self.number), player)
       target_player = target_player.left
     end
   end
