d6b5b0cfabc700188a30c0f50f1974f7b0da3c01
Kevin McHugh <kev@kevinmchugh.me>
2014-06-13 14:26:43 -0500

    Explain how to run the demo game

--- demo_game.rb
+++ demo_game.rb
@@ -1,2 +1,3 @@
+# RUN as 'rails r demo_game.rb'
 require 'attack_left_brain'
 CreateGame.new(brains: [PlayerBrain::AttackLeftBrain,PlayerBrain::AttackLeftBrain,PlayerBrain::AttackLeftBrain,PlayerBrain::AttackLeftBrain]).execute
