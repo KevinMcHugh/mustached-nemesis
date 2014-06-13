a5b8c1da0987e14ff53f3e27fd72e76a38f5a5b3
Kevin McHugh <kev@kevinmchugh.me>
2014-06-13 10:55:23 -0500

    Suzy Lafeyette should obey the player interface

--- app/models/characters/suzy_lafayette.rb
+++ app/models/characters/suzy_lafayette.rb
@@ -1,7 +1,7 @@
 module Character
   class SuzyLafayettePlayer < Player
-    def play_and_discard(card)
-      super(card)
+    def play_and_discard(card, target_player=nil, target_card=nil)
+      super(card, target_player, target_card)
       deck.take(1) if hand_size == 0
     end
   end
