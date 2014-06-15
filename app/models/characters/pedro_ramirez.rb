d81ae945a796ea27e095665bd7d0b7a1dcb10ff9
Mark Grandjean <mark@grandjean.net>
2014-06-15 11:16:49 -0500

    Is Pedro Ramirez done right?

--- app/models/characters/pedro_ramirez.rb
+++ app/models/characters/pedro_ramirez.rb
@@ -1,3 +1,4 @@
+## This is not quite right.  PedroRamirez has to choose the top card without looking at it.  I'm not sure the best way to handle that
 module Character
   class PedroRamirezPlayer < Player
     def draw_for_turn
