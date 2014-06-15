7c43b032d6b561c3152cd9782fd50940437344a5
Mark Grandjean <mark@grandjean.net>
2014-06-15 12:35:58 -0500

    paul regret has 3 health

--- app/models/characters/paul_regret.rb
+++ app/models/characters/paul_regret.rb
@@ -3,6 +3,8 @@ module Character
     def initialize(role, deck, brain=nil)
       super(role, deck, brain)
       @range_increase = 1
+      @max_health = sheriff? ? 4 : 3
+      @health = max_health
     end
   end
 end
