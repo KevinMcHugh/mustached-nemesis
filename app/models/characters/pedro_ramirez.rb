8f53f5ae916db27776ba606df7f72d85a1639b08
Mark Grandjean <mark@grandjean.net>
2014-06-17 17:15:51 -0500

    pedro and bug fix to mildly intellegent brain

--- app/models/characters/pedro_ramirez.rb
+++ app/models/characters/pedro_ramirez.rb
@@ -1,6 +1,13 @@
-## This is not quite right.  PedroRamirez has to choose the top card without looking at it.  I'm not sure the best way to handle that
 module Character
-  class PedroRamirezPlayerTODO < Player
-      # TODO, the top card of the deck or of the discard
+  class PedroRamirezPlayer < Player
+    def draw_for_turn
+      card = brain.draw_choice(deck.most_recently_discarded)
+      if card && card == deck.most_recently_discarded
+        hand << card
+        1.times { draw }
+      else
+        2.times { draw }
+      end
+    end
   end
 end
