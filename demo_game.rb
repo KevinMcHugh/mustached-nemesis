c0f8944c8fa7af8ea8e03a93857d1c33337594ec
Kevin McHugh <kev@kevinmchugh.me>
2014-06-20 13:41:14 -0500

    Prettier logging in demo game

--- demo_game.rb
+++ demo_game.rb
@@ -7,5 +7,7 @@ persist = ARGV.first
 
 game = CreateGame.new(brains: [PlayerBrain::MildlyIntelligentBrain,PlayerBrain::MildlyIntelligentBrain,PlayerBrain::MildlyIntelligentBrain,PlayerBrain::MildlyIntelligentBrain], persist: persist).execute
 
- pp GameRecord.last.event_records.map(&:event_json)
-# game.persist if persist
+event_strings =  GameRecord.last.event_records.map(&:event_json)
+event_strings.each do |string|
+  puts string
+end
