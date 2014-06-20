2f3597b2012ffbaf1785cb904b27c18e586fd3b5
Kevin McHugh <kev@kevinmchugh.me>
2014-06-20 13:40:17 -0500

    Create the event earlier in TargetOfIndians

--- app/models/events/target_of_indians.rb
+++ app/models/events/target_of_indians.rb
@@ -2,12 +2,12 @@ module TargetOfIndians
 
   def target_of_indians(card, targetter)
     response = from_hand_dto_to_card(brain.target_of_indians(card, targetter))
+    Event.new(event_listener, self, targetter)
     if can_play?(response, Card.bang_card)
       discard(response)
     else
       hit!(targetter)
     end
-    Event.new(event_listener, self, targetter)
   end
 
   class Event < ::Event
