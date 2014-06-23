2c7331a178879c1ba4b383b5de95602f4e6f4c62
Kevin McHugh <kev@kevinmchugh.me>
2014-06-22 20:59:28 -0500

    Clean up PersistGame

--- app/services/persist_game.rb
+++ app/services/persist_game.rb
@@ -10,18 +10,29 @@ class PersistGame
 
   def execute
     gr = GameRecord.create(seed: @seed, rounds: @game.round)
+    persist_events(gr)
+    persist_players(gr)
+    persist_expansions(gr)
+  end
+
+  private
+  def persist_events(gr)
     @game.events.each_with_index do |event, index|
       EventRecord.create(game_record_id: gr.id, order: index, event_json: event.to_json)
     end
-    # binding.pry
+  end
+
+  def persist_players(gr)
     @brains.each_with_index do |brain, index|
       won = @game.winners.map(&:class).map(&:to_s).include?(brain.player.character)
       PlayerRecord.create(game_record_id: gr.id, order: index, brain: brain.class.to_s,
         role: brain.role, won: won, character: brain.player.character)
     end
+  end
+
+  def persist_expansions(gr)
     @expansions.each do |expansion|
       Expansion.create(game_record_id: gr.id, name: expansion)
     end
   end
-
 end
