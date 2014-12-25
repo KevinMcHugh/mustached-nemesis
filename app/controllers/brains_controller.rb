ec4267f03e1b363e4bd30955518a623190f23ec7
Kevin McHugh <kev@kevinmchugh.me>
2014-12-25 13:22:46 -0600

    Show what brains do what how much

--- app/controllers/brains_controller.rb
+++ app/controllers/brains_controller.rb
@@ -1,11 +1,6 @@
 class BrainsController < ApplicationController
 
   def index
-    @brains = PlayerBrain.all
-  end
-
-  def show
-    @brain = params[:id].constantize
-    @brain_stats = BrainStats.new(@brain).execute
+    @stats = BrainStats.new.execute
   end
 end
--- app/services/brain_stats.rb
+++ app/services/brain_stats.rb
@@ -1,19 +1,48 @@
 class BrainStats
 
-  def initialize(brain)
-    @brain = brain
-  end
-
   def execute
-    players = player_records
-    events = EventRecord.where(player_record_id: players.pluck(:id))
-    stats = events.group_by(&:eventtype)
-    frequency = stats.map { |stat| {eventtype: stat.first, occurences: stat.second.length, percent: 100 * stat.second.length.to_f / events.length } }
-    frequency.sort_by { |stat| -stat[:occurences]}
-    # binding.pry
+    brains = PlayerBrain.all
+    s = brains.map do |brain|
+      events = events(brain)
+      stats = events.group_by(&:eventtype)
+      frequency = stats.map { |stat| {eventtype: stat.first, occurences: stat.second.length, percent: 100 * stat.second.length.to_f / events.length } }
+      f = frequency.sort_by { |stat| -stat[:occurences]}
+      Status.new(brain, f)
+    end
+    flip(s)
   end
 
-  def player_records
-    PlayerRecord.where(brain: @brain.to_s)
+  def events(brain)
+    players = PlayerRecord.where(brain: brain.to_s)
+    EventRecord.where(player_record_id: players.pluck(:id))
+  end
+
+  def flip(statuses)
+    header = statuses.map do |s|
+      s.brain.to_s
+    end
+    longest_column_length = statuses.max {|s| s.stats.length}.stats.length
+    rows = (0..longest_column_length -1).map do |i|
+
+      statuses.map do |s|
+        s.stats[i] || {}
+      end
+    end
+    Table.new(header, rows)
+  end
+
+  class Status
+    attr_reader :brain, :stats
+    def initialize(brain, stats)
+      @brain = brain
+      @stats = stats
+    end
+  end
+  class Table
+    attr_reader :header, :rows
+    def initialize(header, rows)
+      @header = header
+      @rows = rows
+    end
   end
 end
