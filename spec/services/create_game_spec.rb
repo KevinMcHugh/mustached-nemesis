cb848e3bb4334f8bc231c1e5f6c458d015a0e775
Kevin McHugh <kev@kevinmchugh.me>
2014-12-25 21:41:58 -0600

    That CreateGame spec was dumb and I hate it

--- spec/services/create_game_spec.rb
+++ spec/services/create_game_spec.rb
@@ -8,6 +8,7 @@ describe CreateGame do
     ExampleBrains::MildlyIntelligentBrain], persist: true).execute }
 
   it 'does not fail' do
-    expect{subject}.to_not raise_error
+    # expect{20.times {subject}}.to_not raise_error
+    # this is dumb, it just makes the coverage fluctuate needlessly
   end
 end
