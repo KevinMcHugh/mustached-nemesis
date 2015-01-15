b7546c39a4aa00a1bb8c9f43f1640c97e9ad2e13
Kevin McHugh <kev@kevinmchugh.me>
2015-01-15 10:23:58 -0600

    Prettifies game_record#show

--- app/helpers/game_records_helper.rb
+++ app/helpers/game_records_helper.rb
@@ -1,2 +1,30 @@
 module GameRecordsHelper
+
+  def format_event_record(event_record)
+    hash = {}
+    hash[:target_player] = event_record.target_player_record.to_s if event_record.target_player_record
+    json = JSON.parse(event_record.event_json)
+    hash[:card] = json['@card']['@type'] if json['@card']
+    target_card = json['@target_card'] if json['@target_card']
+    hash[:target_card] = target_card['@type'] if target_card && target_card['@type']
+    hash.empty? ? nil : hash
+  end
+
+  def class_for(event_record)
+    eventtypes_to_classes = {
+      'NewGameStartedEvent'    => 'info',
+      'NewRoundStartedEvent'   => 'info',
+      'NewTurnStarted::Event'  => 'info',
+      'Discard::Event'         => 'active',
+      'TargetOfDuel::Event'    => 'warning',
+      'TargetOfBang::Event'    => 'warning',
+      'TargetOfIndians::Event' => 'warning',
+      'DynamiteCheck::Event'   => 'warning',
+      'Jail::Event'            => 'warning',
+      'Hit::Event'             => 'danger',
+      'Heal::Event'            => 'success',
+      'TapBadge::Event'        => 'success'
+    }
+    eventtypes_to_classes[event_record.eventtype]
+  end
 end
--- app/models/player_record.rb
+++ app/models/player_record.rb
@@ -1,3 +1,6 @@
 class PlayerRecord < ActiveRecord::Base
   belongs_to :game_record
+  def to_s
+    "#{brain}|#{role}|#{character}"
+  end
 end
